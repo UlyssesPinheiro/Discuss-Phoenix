fb905c4846f19d9029e10a79597082f362902529
Ulysses Pinheiro <ulyssespinheiro00@gmail.com>
2022-12-11 17:28:31 -0300

    Sign in and Sign out

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -8,6 +8,7 @@ defmodule DiscussWeb.Router do
     plug :put_root_layout, {DiscussWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug DiscussWeb.Plugs.SetUser
   end
 
   pipeline :api do
@@ -17,7 +18,6 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through :browser
 
-
     # RESTfull conventions
     # get "/", TopicController, :index
     # get "/topics/new", TopicController, :new
@@ -25,6 +25,15 @@ defmodule DiscussWeb.Router do
     # get "/topics/:id/edit", TopicController, :edit
     # put "/topics/:id", TopicController, :update
     resources "/", TopicController
+  end
+
+  scope "/auth", DiscussWeb do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
 
   end
 
